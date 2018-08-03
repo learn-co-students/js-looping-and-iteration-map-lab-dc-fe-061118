a0c586292a7932ff51678973505733a652054ae8
Christine Patton <christine@pattoncreative.com>
2018-08-03 15:50:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(x) {
+let smallCaseDrivers=x.map(lowerCaseDriver);
+return smallCaseDrivers;
+} 
+
+function lowerCaseDriver (x) {
+  return x.toLowerCase();
+}
+
+
+function nameToAttributes (x) {
+  return x.map(separate);
+}
+
+function separate (x) {
+  let fullName = x.split(' ');
+  return {
+    firstName: fullName[0],
+    lastName: fullName[1]
+  };
+}
+
+function attributesToPhrase (arr) {
+  return arr.map(phrase);
+}
+
+function phrase (obj) {
+  return obj.name + " is from " + obj.hometown;
+}
+
